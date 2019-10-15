640b9fd99dfc33db7fc8f381f8bfddbdb10388ca
eunyoungroh <roheunyoung@outlook.com>
2019-10-15 15:50:38 +0900

    실습 7

--- main.c
+++ main.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h> 
-#include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int n);
-
 int main(int argc, char *argv[])
 {
+	// Combination을 계산하는 프로그램 
 	
-	int i;
-	int max = 45;
+	int n;
+	int r;
 	
-//	srand((unsigned) 10); // 같은 정수를 넣으면 항상 같은 패턴이 나옴 
-	srand((unsigned) time(NULL));
-	
-	for(i = 0; i < 6; i++)
-		printf("%d\n", 1 + rand() % max);
+	printf("조합을 계산하는 프로그램입니다.\n");
+	printf("n 값을 입력하세요: ");
+	scanf("%d", &n);
+	printf("r 값을 입력하세요: ");
+	scanf("%d", &r);
+		
+	printf("C(%d, %d)의 값은 %d입니다.", n, r, combination(n, r));
 	
 	return 0;	
+}
+
+int combination(int n, int r){
+	
+	return (factorial(n) / (factorial(n - r) * factorial(r)));
+}
+
+int factorial(int n)
+{
+	int i;
+	int result = 1;
+	
+	for(i = 1; i <= n; i++)
+		result = result * i;
+	
+	return result;
 }
 
