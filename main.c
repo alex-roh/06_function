805d8995106e37fb7cbd26ece7efb5ac36f1c57f
eunyoungroh <roheunyoung@outlook.com>
2019-10-15 15:40:21 +0900

    실습 6
    실습 5는 교수님께서 생략하셨습니다.

--- main.c
+++ main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h> 
+#include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int square(int n);
 
 int main(int argc, char *argv[])
 {
 	
-	int result;
-	result = square(5);
-	printf("%d", result);
+	int i;
+	int max = 45;
+	
+//	srand((unsigned) 10); // 같은 정수를 넣으면 항상 같은 패턴이 나옴 
+	srand((unsigned) time(NULL));
+	
+	for(i = 0; i < 6; i++)
+		printf("%d\n", 1 + rand() % max);
 	
 	return 0;	
 }
 
-int square(int n)
-{
-	return (n * n);	
-}
